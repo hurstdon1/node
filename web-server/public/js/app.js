b49a077909ec175478c711118431003df3892857
hurstdon1 <hurstdon1@gmail.com>
2021-07-08 19:01:49 -0700

    Added some interactive search bar and button elements

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,48 @@
 console.log('Client side Js File is loaded!')
+
+// This script runs in client side JS. Not supported by NODE
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+// The # allows us to get element by the ID like . is used for class
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
+
+// messageOne.textContent = 'From JavaScript'
+
+weatherForm.addEventListener('submit', (e) => {
+    // This prevents default behavior AKA reloading the page
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    messageThree.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            }
+            else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = "Forecast: " + data.forecast.description
+                messageThree.textContent = "Temperature: " + data.forecast.temp + " degrees"
+                console.log(data.forecast)
+            }
+        })
+    })
+
+    // console.log(location)
+})
